454208b640106e7c7689868ad34cffb68e9e55fa
kenriortega <kenriortega@gmail.com>
2020-09-08 01:10:39 +0100

    [REFACTOR] add func para dar el parte epidemiologico

--- bot.py
+++ bot.py
@@ -13,35 +13,57 @@ import json
 load_dotenv()
 
 
-def start(update, context):
+def clima(update, context):
     keyboard = [
         [
             InlineKeyboardButton(
                 "Playa",
-                callback_data='playa',
+                callback_data='wh_playa',
             ),
             InlineKeyboardButton(
                 "Centro Habana",
-                callback_data='centro habana',
+                callback_data='wh_centro habana',
             ),
             InlineKeyboardButton(
                 "Habana del Este",
-                callback_data='habana del este',
+                callback_data='wh_habana del este',
             ),
         ],
 
         [
             InlineKeyboardButton(
                 "Diez de Octubre",
-                callback_data='diez de octubre',
+                callback_data='wh_diez de octubre',
             ),
             InlineKeyboardButton(
                 "Cerro",
-                callback_data='cerro',
+                callback_data='wh_cerro',
             ),
             InlineKeyboardButton(
                 "Plaza de la revolucion",
-                callback_data='plaza de la revolucion',
+                callback_data='wh_plaza de la revolucion',
+            ),
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
+def covid19_prov(update, context):
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "La Habana",
+                callback_data='cv_lha',
+            ),
+            InlineKeyboardButton(
+                "Artemisa",
+                callback_data='cv_art',
+            ),
+            InlineKeyboardButton(
+                "Camaguey",
+                callback_data='cv_cam',
             ),
         ],
     ]
@@ -53,27 +75,64 @@ def start(update, context):
 def help_command(update, context):
     name = update.message.from_user.username
 
-    update.message.reply_text(f"Hi {name} Use /start to test this bot.")
+    update.message.reply_text(f"""
+    Hi {name} Use this commands:
+
+    /covid19 to get status in your municipality.
+
+    /clima to get wheather in your locale
+                              """)
 
 
-def button(update, context):
+def get_acction_buttom(update, context):
     query = update.callback_query
-
     query.answer()
-    r = requests.get(
-        f"https://cuba-weather-serverless.vercel.app/api/get-weather?name={query.data}",
-    )
-    query.edit_message_text(text=f"""
-                    El tiempo en {query.data} es:
-        tempC: {r.json().get('tempC')}
-        tempF: {r.json().get('tempF')}
-        Humedad: {r.json().get('humidity')}
-        Presion: {r.json().get('pressure')}
-        Time: {r.json().get('timestamp')}
-        Viento: {r.json().get('windDirectionDescription')}
-        Descripcion: {r.json().get('descriptionWeather')}
-        icon: {r.json().get('iconWeather')}
-                            """)
+
+    try:
+        data = json.loads(query.data)
+    except json.JSONDecodeError:
+        data = query.data
+
+    if 'wh' in data.split("_"):
+        municipio = data.split("_")[1]
+        r = requests.get(
+            f"https://cuba-weather-serverless.vercel.app/api/get-weather?name={municipio}",
+        )
+        query.edit_message_text(text=f"""
+            El tiempo en {data.split("_")[1]} es:
+            tempC: {r.json().get('tempC')}
+            tempF: {r.json().get('tempF')}
+            Humedad: {r.json().get('humidity')}
+            Presion: {r.json().get('pressure')}
+            Time: {r.json().get('timestamp')}
+            Viento: {r.json().get('windDirectionDescription')}
+            Descripcion: {r.json().get('descriptionWeather')}
+            icon: {r.json().get('iconWeather')}
+                                """)
+    elif 'cv' in data.split("_"):
+        prov = data.split("_")[1]
+
+        r = requests.get(
+            "https://covid19cuba.github.io/covid19cubadata.github.io/api/v2/full.json",
+        )
+
+        provincia = r.json().get('provinces')[prov].get('all')
+        fecha_updated = provincia.get('updated')
+        affected = provincia.get(
+            'deceases_affected_municipalities')
+        text_affected = ""
+        for af in affected:
+            text_affected += f"""
+            #####
+            Name: {af.get('name')}
+            Cant: {af.get('value')}
+            #####
+            """
+        query.edit_message_text(text=f"""
+                        Este es el ultimo pronostico del dia {fecha_updated} en {prov}:
+
+                        {text_affected}
+                        """)
 
 
 def main():
@@ -81,9 +140,11 @@ def main():
     #     file_path="/home/pi/proyects/bot_pi_wheather/secrets.yaml")
     updater = Updater(token=os.getenv('TELEGRAM_TOKEN'), use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('clima', clima))
+    dp.add_handler(CommandHandler('covid19', covid19_prov))
+    dp.add_handler(CallbackQueryHandler(get_acction_buttom))
+    dp.add_handler(CommandHandler('help', help_command))
 
     # Start BOT
     updater.start_polling()
