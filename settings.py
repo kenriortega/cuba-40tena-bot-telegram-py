afe7d2c5d77712fd39f7ea740f474dd2bee6f773
kenriortega <kenriortega@gmail.com>
2020-09-11 23:06:03 +0100

    [ADD] lib redis and settings

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
 import yaml
 from dataclasses import dataclass
+import os
+redis_config = {
+    "host": os.getenv('REDIS_URI'),
+    "port": os.getenv('REDIS_PORT'),
+    "password": os.getenv('REDIS_PASS'),
+    "db": 0,
+}
 
 
 @dataclass
